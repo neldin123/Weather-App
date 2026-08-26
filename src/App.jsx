7f142ddfc6e05484fde5482d15fa483e91ed083a
Nooreldin Abdelnaser <nbdalnasr52@gmail.com>
2026-08-27 01:08:01 +0300

    Add Arabic/English localization
    This change introduces i18next-based language switching for the weather app, adds English and Arabic locale files, and updates the UI to render translated labels and RTL/LTR layout based on the selected language.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/set-state-in-effect */
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./App.css";
 
@@ -14,8 +16,7 @@ import Button from "@mui/material/Button";
 import axios from "axios";
 import moment from "moment/moment";
 import "moment/locale/ar";
-
-moment.locale("ar");
+import { useTranslation } from "react-i18next";
 
 const theme = createTheme({
   typography: {
@@ -28,8 +29,8 @@ const apiKey = import.meta.env.VITE_WEATHER_APP_KEY;
 let cancelAxios = null;
 
 const App = () => {
-  // const dateAndTime = moment().format("MMMM Do YYYY, h:mm:ss a");
-  const [dateAndTime] = useState(() => moment().format("Do MMMM  YYYY"));
+  const { t, i18n } = useTranslation();
+  const [dateAndTime, setDateAndTime] = useState("");
   const [temp, setTemp] = useState({
     currtemp: null,
     mintemp: null,
@@ -37,7 +38,11 @@ const App = () => {
     description: null,
     icon: null,
   });
+  const [direction, setDirection] = useState("");
   useEffect(() => {
+    moment.locale("en");
+    setDateAndTime(moment().format("Do MMMM  YYYY"));
+    i18n.changeLanguage("en");
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?lat=30.0444&lon=31.2357&units=metric&appid=${apiKey}`,
@@ -71,6 +76,20 @@ const App = () => {
     };
   }, []);
 
+  function handleLanguageClick() {
+    if (i18n.language === "en") {
+      i18n.changeLanguage("ar");
+      setDirection("rtl");
+      moment.locale("ar");
+      setDateAndTime(moment().format("Do MMMM  YYYY"));
+    } else {
+      i18n.changeLanguage("en");
+      setDirection("ltr");
+      moment.locale("en");
+      setDateAndTime(moment().format("Do MMMM  YYYY"));
+    }
+  }
+
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
@@ -97,7 +116,7 @@ const App = () => {
               <div className="content">
                 <div>
                   <div
-                    dir="rtl"
+                    dir={direction}
                     style={{
                       display: "flex",
                       alignItems: "end",
@@ -105,7 +124,7 @@ const App = () => {
                     }}
                   >
                     <Typography variant="h2" sx={{ fontWeight: "600" }}>
-                      القاهرة
+                      {t("القاهرة")}
                     </Typography>
                     <Typography variant="h5">{dateAndTime}</Typography>
                   </div>
@@ -115,7 +134,7 @@ const App = () => {
                 <div
                   style={{
                     display: "flex",
-                    direction: "rtl",
+                    direction: direction,
                     justifyContent: "space-around",
                   }}
                 >
@@ -143,9 +162,13 @@ const App = () => {
                         alignItems: "center",
                       }}
                     >
-                      <h4>الصغرى: {temp.mintemp}</h4>
-                      <h4 style={{ margin: "0 7px" }}>|</h4>
-                      <h4>الكبرى: {temp.maxtemp}</h4>
+                      <h4>
+                        {t("الصغرى")}: {temp.mintemp}
+                      </h4>
+                      <h4 style={{ margin: "0 5px" }}>|</h4>
+                      <h4>
+                        {t("الكبرى")}: {temp.maxtemp}
+                      </h4>
                     </div>
                   </div>
                   <Cloud sx={{ fontSize: "200px", color: "white" }} />
@@ -158,11 +181,15 @@ const App = () => {
                 justifyContent: "end",
                 alignItems: "center",
                 width: "100%",
-                direction: "rtl",
+                direction: direction,
               }}
             >
-              <Button variant="text" sx={{ color: "white", marginTop: "20px" }}>
-                إنجليزي
+              <Button
+                variant="text"
+                sx={{ color: "white", marginTop: "20px" }}
+                onClick={handleLanguageClick}
+              >
+                {t("English")}
               </Button>
             </div>
           </div>
---
+++ test.js
@@ -0,0 +1,17 @@
+console.log("1");
+
+setTimeout(() => {
+  console.log("2");
+}, 0);
+
+Promise.resolve()
+  .then(() => {
+    console.log("3");
+  })
+  .then(() => {
+    console.log("4");
+  });
+
+queueMicrotask(() => {
+  console.log("5");
+});
